3f1c7f10d0b3a9cd3e2df5589620e11c8a80dc15
Marcus Sucilea <47612279+MarcusElisei@users.noreply.github.com>
2020-04-20 13:58:28 +0100

    Contact Manager - C#, Asp .Net Core MVC, Visual Studio IDE

---
+++ HarrisandSonsContactWeb/HarrisandSonsContactWeb/Data/DbInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrisandSonsContactWeb.Data
+{
+    /// <summary>
+    /// This class creates the local database
+    /// </summary>
+    public class DbInitializer
+    {
+        public static void Initialize(HarrisDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
---
+++ HarrisandSonsContactWeb/HarrisandSonsContactWeb/Data/HarrisDbContext.cs
@@ -0,0 +1,24 @@
+﻿using HarrisandSonsContactWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrisandSonsContactWeb.Data
+{
+    /// <summary>
+    /// This class creates the database based on the classes PersonalContact and Business Contact
+    /// and it inherits the DbContext class
+    /// </summary>
+    public class HarrisDbContext : DbContext
+    {
+        public HarrisDbContext(DbContextOptions<HarrisDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<PersonalContact> PersonalContacts { get; set; }
+        public DbSet<BusinessContact> BusinessContacts { get; set; }
+
+    }
+}
